1558f1044cd52a41803c08790c9efead9c403f3c
papijo <joe.ebh100@live.com>
2022-02-07 14:35:30 +0100

    First Commit

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+const connectDB =  async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB Connection Success!!!');
+    }catch(error) {
+        console.error('MongoDB Connection Fail!!!!');
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+connectDB();
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on PORT ${PORT}`);
+})
